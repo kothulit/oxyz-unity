4a205336e9b0f52530b420593d7f7c70ef5dbfde
Aleksei Ponomarev <kothulit@gmail.com>
2026-04-09 18:35:48 +0500

    Изменил логику построения меша, теперь каждая грань имеет свои вершины. Добавил ресурс для TMP

--- Assets/Objects/Mesh/ExtrusionMeshBuilder.cs
+++ Assets/Objects/Mesh/ExtrusionMeshBuilder.cs
@@ -8,77 +8,130 @@ public static class ExtrusionMeshBuilder
         var contour = ExtractOrderedContourPoints(geometry);
         if (contour.Count < 3)
             return null;
+
         float yBottom = geometry.InsertPoint.Y + geometry.BottomOffset;
         float yTop = geometry.InsertPoint.Y + geometry.TopOffset;
+
         var vertices = new List<Vector3>();
         var triangles = new List<int>();
-        int n = contour.Count;
-        // Bottom vertices
-        for (int i = 0; i < n; i++)
-        {
-            var p = contour[i];
-            vertices.Add(new Vector3(
-                geometry.InsertPoint.X + p.X,
-                yBottom,
-                geometry.InsertPoint.Z + p.Y));
-        }
-        // Top vertices
-        for (int i = 0; i < n; i++)
-        {
-            var p = contour[i];
-            vertices.Add(new Vector3(
-                geometry.InsertPoint.X + p.X,
-                yTop,
-                geometry.InsertPoint.Z + p.Y));
-        }
-        // Bottom face
-        for (int i = 1; i < n - 1; i++)
-        {
-            triangles.Add(0);
-            triangles.Add(i);
-            triangles.Add(i + 1);
-        }
-        // Top face
-        int topOffset = n;
-        for (int i = 1; i < n - 1; i++)
-        {
-            triangles.Add(topOffset + 0);
-            triangles.Add(topOffset + i + 1);
-            triangles.Add(topOffset + i);
 
-        }
-        // Side faces
-        for (int i = 0; i < n; i++)
-        {
-            int next = (i + 1) % n;
-            int b0 = i;
-            int b1 = next;
-            int t0 = topOffset + i;
-            int t1 = topOffset + next;
-            triangles.Add(b0);
-            triangles.Add(t0);
-            triangles.Add(t1);
-            triangles.Add(b0);
-            triangles.Add(t1);
-            triangles.Add(b1);
-        }
+        AddBottomFace(vertices, triangles, contour, geometry.InsertPoint, yBottom);
+        AddTopFace(vertices, triangles, contour, geometry.InsertPoint, yTop);
+        AddSideFaces(vertices, triangles, contour, geometry.InsertPoint, yBottom, yTop);
+
         var mesh = new Mesh();
         mesh.name = geometry.Id ?? "ExtrusionMesh";
         mesh.SetVertices(vertices);
         mesh.SetTriangles(triangles, 0);
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
+
         return mesh;
     }
+
+    private static void AddBottomFace(
+        List<Vector3> vertices,
+        List<int> triangles,
+        List<Point2D> contour,
+        Point3D insertPoint,
+        float yBottom)
+    {
+        int startIndex = vertices.Count;
+
+        for (int i = 0; i < contour.Count; i++)
+        {
+            var p = contour[i];
+            vertices.Add(new Vector3(
+                insertPoint.X + p.X,
+                yBottom,
+                insertPoint.Z + p.Y));
+        }
+
+        for (int i = 1; i < contour.Count - 1; i++)
+        {
+            triangles.Add(startIndex + 0);
+            triangles.Add(startIndex + i);
+            triangles.Add(startIndex + i + 1);
+        }
+    }
+
+    private static void AddTopFace(
+        List<Vector3> vertices,
+        List<int> triangles,
+        List<Point2D> contour,
+        Point3D insertPoint,
+        float yTop)
+    {
+        int startIndex = vertices.Count;
+
+        for (int i = 0; i < contour.Count; i++)
+        {
+            var p = contour[i];
+            vertices.Add(new Vector3(
+                insertPoint.X + p.X,
+                yTop,
+                insertPoint.Z + p.Y));
+        }
+
+        for (int i = 1; i < contour.Count - 1; i++)
+        {
+            triangles.Add(startIndex + 0);
+            triangles.Add(startIndex + i + 1);
+            triangles.Add(startIndex + i);
+        }
+    }
+
+    private static void AddSideFaces(
+        List<Vector3> vertices,
+        List<int> triangles,
+        List<Point2D> contour,
+        Point3D insertPoint,
+        float yBottom,
+        float yTop)
+    {
+        int count = contour.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            int next = (i + 1) % count;
+
+            var p0 = contour[i];
+            var p1 = contour[next];
+
+            Vector3 b0 = new Vector3(insertPoint.X + p0.X, yBottom, insertPoint.Z + p0.Y);
+            Vector3 b1 = new Vector3(insertPoint.X + p1.X, yBottom, insertPoint.Z + p1.Y);
+            Vector3 t0 = new Vector3(insertPoint.X + p0.X, yTop, insertPoint.Z + p0.Y);
+            Vector3 t1 = new Vector3(insertPoint.X + p1.X, yTop, insertPoint.Z + p1.Y);
+
+            int startIndex = vertices.Count;
+
+            vertices.Add(b0);
+            vertices.Add(b1);
+            vertices.Add(t1);
+            vertices.Add(t0);
+
+            triangles.Add(startIndex + 0);
+            triangles.Add(startIndex + 2);
+            triangles.Add(startIndex + 1);
+
+            triangles.Add(startIndex + 0);
+            triangles.Add(startIndex + 3);
+            triangles.Add(startIndex + 2);
+        }
+    }
+
     private static List<Point2D> ExtractOrderedContourPoints(ExtrusionGeometry geometry)
     {
         var result = new List<Point2D>();
+
         if (geometry?.Contour == null || geometry.Contour.Count == 0)
             return result;
+
         for (int i = 0; i < geometry.Contour.Count; i++)
         {
             result.Add(geometry.Contour[i].Start);
         }
+
         return result;
     }
 }
