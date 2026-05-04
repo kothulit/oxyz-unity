092614f192a47347384ecbffd1486e438012bc9c
Aleksei Ponomarev <kothulit@gmail.com>
2026-05-04 18:04:36 +0500

    Добавил в Element флаги назначения свойств

--- Assets/Application/Scripts/XML/Serializable/Element.cs
+++ Assets/Application/Scripts/XML/Serializable/Element.cs
@@ -7,12 +7,21 @@ namespace Oxyz.Xml.Serializable
     public class Element
     {
         [XmlAttribute("name")]
-        public string Name { get; set; } = "NewElement";
+        public string Name { get; set; }
+
+        [XmlIgnore]
+        public bool NameSpecified { get; set; }
 
         [XmlAttribute("guid")]
-        public string GUID { get; set; } = Guid.NewGuid().ToString();
+        public string GUID { get; set; }
+
+        [XmlIgnore]
+        public bool GuidSpecified { get; set; }
 
         [XmlAttribute("style")]
         public string Style { get; set; }
+
+        [XmlIgnore]
+        public bool StyleSpecified { get; set; }
     }
 }
